4cdcef7e01bb8009332b9fe0365125dbffe760c0
Sean-Crowley <seancrowley033@gmail.com>
2025-01-22 13:54:21 +0000

    Enemy will now rotate towards player in alert state

--- scripts/enemy/states/enemy_alert.gd
+++ scripts/enemy/states/enemy_alert.gd
@@ -6,7 +6,10 @@ func process_physics(delta: float) -> State:
 	var destination  = parent.navigation_agent_3d.get_next_path_position()
 	var local_destination = destination - parent.global_position
 	var direction = local_destination.normalized()
-		
+	
+	#parent.look_at(direction + destination,Vector3.UP)
+	look_at_player(0.2, direction + destination)
+
 	parent.velocity  = direction * parent.SPEED
 	return null
 
@@ -20,3 +23,9 @@ func _on_detection_body_entered(body):
 
 func _on_navigation_agent_3d_target_reached():
 	print("HIT")
+	
+func look_at_player(weight,target):
+	var xform := parent.transform
+	xform = xform.looking_at(target,Vector3.UP)
+	parent.transform = parent.transform.interpolate_with(xform,weight)
+	
