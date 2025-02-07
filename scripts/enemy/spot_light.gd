30118204e6f15c027a3dff8447dc7fc8d059371e
Sean-Crowley <seancrowley033@gmail.com>
2025-02-07 17:05:32 +0000

    Spotlight now rotated between two angles

--- scripts/enemy/spot_light.gd
+++ scripts/enemy/spot_light.gd
@@ -2,7 +2,15 @@ extends CharacterBody3D
 
 
 @onready var light = $Light
+var rotation_speed = .2
 
 func _physics_process(delta) -> void:
+	rotate_spotlight(delta)
 	
-	light.rotation.y += .25 * delta
+func rotate_spotlight(delta):
+	light.rotation.y += rotation_speed * delta
+
+	if light.rotation_degrees.y > 75:
+		rotation_speed = -rotation_speed
+	elif light.rotation_degrees.y < -75:
+		rotation_speed = rotation_speed * -1
