73a34b93f92637c3759f3edc1426810a7adf902e
Sean-Crowley <seancrowley033@gmail.com>
2025-06-02 15:14:30 +0100

    Player can exit vehicle

--- scripts/vehicle_body_3d.gd
+++ scripts/vehicle_body_3d.gd
@@ -7,22 +7,38 @@ const ENGINE_POWER = 300
 @export var camera_3d : Camera3D
 @export var player : Player
 @export var player_pos: Marker3D
+@export var exit_timer: Timer
+
 var overlapping : bool = false
 var active : bool = false
+var can_exit : bool = false
 
 func _physics_process(delta):
-	if overlapping && Input.is_action_just_pressed("interact"):
+	enter_vehicle(delta)
+	exit_vehicle()
+	
+func enter_vehicle(delta):
+	if overlapping && Input.is_action_just_pressed("interact") && !active:
 		active = true
-		camera_3d.current = true
+		exit_timer.start()
 	if active:
 		vehicle_movement(delta)
+		camera_3d.current = true
 		
+func exit_vehicle():
+	if active && can_exit:
+		if Input.is_action_just_pressed("interact"):
+			active = false
+			camera_3d.current = false
+			can_exit = false
 
 func vehicle_movement(delta):
 	steering = move_toward(steering, Input.get_axis("right","left") * MAX_STEER, delta * 2.5)
 	engine_force = Input.get_axis("back","forward") * ENGINE_POWER
 	player.position = player_pos.global_position
-	
+	#Stops vehcile from accelerating if player exits vehcile
+	if Input.is_action_just_pressed("interact"):
+		engine_force = 0
 
 func _on_interact_area_3d_body_entered(body):
 	if body.name == "Player":
@@ -31,3 +47,6 @@ func _on_interact_area_3d_body_entered(body):
 func _on_interact_area_3d_body_exited(body):
 	if body.name == "Player":
 		overlapping = false
+
+func _on_vehicle_timer_timeout():
+	can_exit = true
